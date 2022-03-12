d376019d1ab8844ffd6adcb9b36d53d47290ea25
Vivek <viveksingh.singh50@gmail.com>
2022-03-12 18:16:20 +0530

    basic Layout of app

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistics } from "antd";
+import { Link } from "react-router-dom";
+
+const Homepage = () => {
+  return <div>Homepage</div>;
+};
+
+export default Homepage;
