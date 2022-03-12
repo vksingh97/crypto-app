71cfa26f62029edd84a9fffe67aabad12fd14c39
Vivek <viveksingh.singh50@gmail.com>
2022-03-12 23:58:51 +0530

    added redux

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { millify } from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const Cryptocurrencies = () => {
-  return <div>Cryptocurrencies</div>;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  return (
+    <>
+      <Row gutters={[32, 32]} className="crypto-card-container"></Row>
+    </>
+  );
 };
 
 export default Cryptocurrencies;
